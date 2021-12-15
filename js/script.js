651c9e9a57064a067d574aa0192e0610656cd2ba
paulaami <pmilkowska@onet.pl>
2021-12-15 18:58:09 +0100

    updated files

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-const formDescription = document.querySelector(".form__description");
-const validationText = document.querySelector(".validation__text");
-const validationLetters = document.querySelector(".validation__letters");
-const validationNumbers = document.querySelector('.validation__numbers');
-const priceNetto = document.querySelector(".price__netto");
-const priceBrutto = document.querySelector(".price__brutto");
-const vatRate = document.querySelector(".vat__rate");
-const allInputs = Array.from(document.querySelectorAll(".all-inputs"));
+const vatForm = document.querySelector('.form');
+
+const formDescription = vatForm.querySelector(".form__description");
+const validationText = vatForm.querySelector(".validation__text");
+const validationRequired = vatForm.querySelector(".validation__required");
+const validationNumbers = vatForm.querySelector('.validation__numbers');
+const priceNetto = vatForm.querySelector("#price-netto");
+const priceBrutto = vatForm.querySelector("#price-brutto");
+const vatRate = vatForm.querySelector(".vat__rate");
+const submitBtn = vatForm.querySelector('.form__button');
+const inputConfirm = vatForm.querySelector('#confirmation');
+const textConfirm = vatForm.querySelector('.validation__confirm');
+const validationVat = vatForm.querySelector('.validation__vat');
+const textRequired = vatForm.querySelector('.text-required');
+const allInputs = Array.from(vatForm.querySelectorAll(".all-inputs"));
+
 
 const maxLength = 255;
-const letters = /^[A-Za-z]+$/;
 const priceRegex = /^\d+([\,]\d+)*([\.]\d+)?$/;
 priceNetto.disabled = true;
 
@@ -20,17 +27,12 @@ const textValidation = () => {
 
  if (formDescriptionValue.length == 0) {
   validationText.textContent = "";
+ } else if (formDescriptionValue.length >= 1 && formDescriptionValue.length < maxLength) {
+  formDescription.classList.remove("warning-border");
+  validationRequired.textContent = "";
  } else if (formDescriptionValue.length >= maxLength) {
   formDescriptionValue = formDescriptionValue.substring(0, maxLength);
   validationText.textContent = "You can’t enter more than 255 characters";
- }
-
- if (/\d/.test(formDescriptionValue)) {
-  formDescription.classList.add("warning-border");
-  validationLetters.textContent = "Text is required";
- } else {
-  formDescription.classList.remove("warning-border");
-  validationLetters.textContent = "";
  }
 }
 
@@ -39,23 +41,28 @@ const vatValidation = () => {
  let vatValue = vatRate.value;
  if (vatValue !== "choose") {
   priceNetto.disabled = false;
+  validationVat.textContent = "";
  } else if (vatValue === "choose") {
   priceNetto.disabled = true;
  }
 }
 
 // NETTO INPUT VALIDATION
-// SHOULD BE ONLY POSITIVE NUMBERS? 
 const nettoValidation = () => {
  let nettoValue = priceNetto.value;
- if (nettoValue.match(priceRegex) || nettoValue == '') {
-  priceNetto.classList.remove("warning-border");
-  validationNumbers.textContent = "";
+ if (!nettoValue.match(priceRegex) || nettoValue.length < 1) {
+  validationNumbers.textContent = "Please, enter correct number";
  } else {
-  priceNetto.classList.add("warning-border");
-  validationNumbers.textContent = "Please, enter input number";
+  validationNumbers.textContent = "";
+  priceNetto.classList.remove("warning-border");
+ }
+
+ if (nettoValue.length >= 1) {
+  textRequired.textContent = '';
  }
 }
+
+
 
 // BRUTTO INPUT VALIDATION
 const bruttoValidation = () => {
@@ -68,14 +75,87 @@ const bruttoValidation = () => {
  }
 }
 
+//RADIO INPUT CHECK
+const radioValidation = () => {
+ if (inputConfirm.checked) {
+  textConfirm.textContent = "";
+ }
+}
+
 const inputsValidation = () => {
  textValidation()
  vatValidation()
  nettoValidation()
  bruttoValidation()
+ radioValidation()
 }
 
+// CHECK INPUTS WHILE TYPING
 allInputs.forEach((input) => {
  input.addEventListener('input', inputsValidation)
 })
 
+/*check if textarea is empty */
+const checkText = () => {
+ if (formDescription.value == '') {
+  formDescription.classList.add("warning-border");
+  validationRequired.textContent = "Text is required";
+ }
+}
+
+/*check if radio is checked */
+const checkConfirm = () => {
+ if (!inputConfirm.checked) {
+  textConfirm.textContent = "This field is required"
+ }
+}
+
+const checkVat = () => {
+ if (vatRate.value == "choose") {
+  validationVat.textContent = "Please choose correct VAT rate";
+ }
+}
+
+/*check if price netto is correct*/
+const checkNetto = () => {
+ if (vatRate.value !== "choose" && priceNetto.value == '') {
+  priceNetto.classList.add("warning-border");
+  textRequired.textContent = "This field is required";
+ }
+}
+
+const showGreenBox = () => {
+ const greenBox = document.querySelector(".green-box");
+ const greenBtn = greenBox.querySelector(".green-box-btn");
+ greenBox.classList.add("show-greenbox");
+}
+
+
+/*check if all is correct */
+const checkAll = () => {
+ const allWarnings = Array.from(vatForm.querySelectorAll('.warning-text'));
+
+ let countWarnings = 0;
+ allWarnings.forEach((element) => {
+  if(element.innerHTML !== "") {
+   countWarnings++
+  }
+ })
+ if(countWarnings === 0) {
+  showGreenBox();
+ }
+}
+// CHECK INPUTS BEFORE SUBMIT
+const checkBeforeSubmit = () => {
+ checkText();
+ checkConfirm();
+ checkVat();
+ checkNetto();
+ checkAll();
+}
+
+submitBtn.addEventListener('click', (event) => {
+ event.preventDefault();
+
+ checkBeforeSubmit();
+})
